632343b10426300aead0d9695946b549254197d2
Nathan Armstrong <kenogu.labz@gmail.com>
2019-03-02 17:49:33 -0600

    Improve guild upgrade code by Rubocop recs.

--- guild_upgrades.rb
+++ guild_upgrades.rb
@@ -1,8 +1,13 @@
+# frozen_string_literal: true
+
 require 'set'
 require './gw2_api'
 require './text_format'
 
+# Guild Wars 2 scope
 module GW2
+  # An event handler that emits info about the guild with the given name to the event's originating
+  # channel, leveraging the given GW2 API token.
   class GuildInfoHandler
     def initialize(guild_name, token)
       @guild_name = guild_name
@@ -36,6 +41,8 @@ module GW2
     attr_reader :guild_name, :token
   end
 
+  # An event handler that emits a list of available upgrades for the guild with the given name to
+  # the event's originating channel, leveraging the given GW2 API token.
   class GuildUpgradeListHandler
     def initialize(guild_name, token)
       @guild_name = guild_name
@@ -56,13 +63,14 @@ module GW2
       upgrades_in_progress =
         GW2::API::GuildUpgradeCatalogMany.request ids: upgrades_in_progress_ids
 
-      upgrades_by_type = upgrades_in_progress
-        .collect { |u| u.values_at(:type, :name) }
+      upgrades_by_type =
+        upgrades_in_progress
+        .map { |u| u.values_at(:type, :name) }
         .group_by { |u| u[0] }
         .sort
-        .collect do |kv|
+        .map do |kv|
           type, upgrades = kv
-          "**#{type}**\n" + (upgrades.collect { |u| u[1] }.sort.collect { |u| "- #{u}" } * "\n")
+          "**#{type}**\n" + (upgrades.map { |u| u[1] }.sort.map { |u| "- #{u}" } * "\n")
         end * "\n\n"
 
       event << upgrades_by_type
@@ -73,7 +81,11 @@ module GW2
     attr_reader :guild_name, :token
   end
 
+  # An event handler that emits progress info about the upgrade with the given name for the guild
+  # with the given name to the event's originating channel, leveraging the given GW2 API token.
   class GuildUpgradeInfoHandler
+    # An event handler that displays upgrades using the given ingredient for the guild with the
+    # given name to the event's originating channel, using the given GW2 API token.
     def initialize(guild_name, upgrade_name, token)
       @guild_name = guild_name
       @upgrade_name = upgrade_name
@@ -95,9 +107,13 @@ module GW2
       upgrades_in_progress_ids = treasury.deep_select(:upgrade_id).uniq! * ','
       upgrades_in_progress = GW2::API::GuildUpgradeCatalogMany.request ids: upgrades_in_progress_ids
 
-      matched_upgrades = upgrades_in_progress.deep_locate -> (key, value, obj) { key == :name && value.include?(upgrade_name) }
+      matched_upgrades = upgrades_in_progress.deep_locate lambda do |key, value, _|
+        key == :name && value.include?(upgrade_name)
+      end
+
       if matched_upgrades.empty?
-        event << "Couldn't find any upgrades similar to '#{upgrade_name}' in progress for '#{guild_name}'"
+        event <<
+          "Couldn't find any upgrades similar to '#{upgrade_name}' in progress for '#{guild_name}'"
         return
       elsif matched_upgrades.count > 1
         upgrade_names = matched_upgrades.map { |u| u[:name] }.sort * ', '
@@ -107,11 +123,12 @@ module GW2
       upgrade = matched_upgrades[0]
 
       costs = upgrade[:costs]
-      item_costs = costs.select { |c| c[:type] == "Item" }
-      non_item_costs = costs.reject { |c| c[:type] == "Item" }
+      item_costs = costs.select { |c| c[:type] == 'Item' }
+      non_item_costs = costs.reject { |c| c[:type] == 'Item' }
 
       item_cost_ids = item_costs.map { |c| c[:item_id] }.to_set
-      items_owned = treasury
+      items_owned =
+        treasury
         .select { |t| item_cost_ids.include?(t[:item_id]) }
         .map { |t| t.values_at :item_id, :count }
         .to_h
@@ -145,9 +162,13 @@ module GW2
             name: c[:name],
             value: TextFormat::GradientProgressBar.new(
               progress: c[:owned],
-              max: c[:needed]).render)
+              max: c[:needed]
+            ).render
+          )
         end
-        embed.add_field(name: 'Complete!', value: 'This upgrade is ready to build!') if item_costs.empty? && non_item_costs.empty?
+        if item_costs.empty? && non_item_costs.empty?
+          embed.add_field(name: 'Complete!', value: 'This upgrade is ready to build!')
+        end
         embed
       end
     end
@@ -157,6 +178,8 @@ module GW2
     attr_reader :guild_name, :upgrade_name, :token
   end
 
+  # An event handler that displays guild treasury contributions during the last day for the guild
+  # with the given name to the event's originating channel, using the given GW2 API token.
   class GuildContributionsHandler
     def initialize(guild_name, token)
       @guild_name = guild_name
@@ -174,24 +197,25 @@ module GW2
       log = GW2::API::GuildLog.request id: guild_id, token: token
       time_limit = DateTime.now.prev_day(1)
 
-      contributions = log
+      contributions =
+        log
         .select { |l| l[:type] == 'treasury' }
         .select { |l| DateTime.iso8601(l[:time]) >= time_limit }
-        .reduce({}) do |acc, l|
+        .each_with_object({}) do |acc, l|
           item_id = l[:item_id]
-          item_hash = acc.fetch(item_id, {count: 0})
+          item_hash = acc.fetch(item_id, count: 0)
           item_hash[:count] = item_hash[:count] + l[:count]
           acc[item_id] = item_hash
-          acc
         end
 
       if contributions.empty?
-        event << "In the last day, there have been no guild contributions. :slight_frown:"
+        event << 'In the last day, there have been no guild contributions. :slight_frown:'
         return
       end
 
       item_ids = contributions.keys * ','
-      GW2::API::ItemCatalogMany.request(ids: item_ids)
+      GW2::API::ItemCatalogMany
+        .request(ids: item_ids)
         .each do |item|
           item_id = item[:id]
           item_name = item[:name]
@@ -200,7 +224,7 @@ module GW2
 
       event << (
         "In the last day, guild members have contributed:\n" +
-        (contributions.values.collect { |c| "#{c[:name]} - #{c[:count]}" }.to_a * "\n"))
+        (contributions.values.map { |c| "#{c[:name]} - #{c[:count]}" }.to_a * "\n"))
     end
 
     private
@@ -208,6 +232,8 @@ module GW2
     attr_reader :guild_name, :upgrade_name, :token
   end
 
+  # An event handler that displays upgrades using the given ingredient for the guild with the given
+  # name to the event's originating channel, using the given GW2 API token.
   class GuildIngredientUpgradesHandler
     def initialize(guild_name, item_name, token)
       @guild_name = guild_name
@@ -225,7 +251,7 @@ module GW2
 
       treasury = GW2::API::GuildTreasury.request id: guild_id, token: token
 
-      treasury_item_ids = treasury.collect { |i| i[:item_id] } * ","
+      treasury_item_ids = treasury.map { |i| i[:item_id] } * ','
       treasury_items = GW2::API::ItemCatalogMany.request ids: treasury_item_ids
 
       matched_items = treasury_items.select { |i| i[:name].include?(item_name) }
@@ -241,13 +267,11 @@ module GW2
       item_id = item[:id]
 
       treasury_item = treasury.select { |i| i[:item_id] == item_id }.first
-      upgrade_ids = treasury_item[:needed_by].collect { |u| u[:upgrade_id] } * ","
-      upgrades_by_id = GW2::API::GuildUpgradeCatalogMany
+      upgrade_ids = treasury_item[:needed_by].map { |u| u[:upgrade_id] } * ','
+      upgrades_by_id =
+        GW2::API::GuildUpgradeCatalogMany
         .request(ids: upgrade_ids)
-        .inject({}) do |m, u|
-          m[u[:id]] = u
-          m
-        end
+        .each_with_object({}) { |m, u| m[u[:id]] = u }
 
       total_needed = treasury_item[:needed_by].inject(0) { |sum, u| sum + u[:count] }
 
@@ -255,14 +279,16 @@ module GW2
         embed.title = item[:name]
         embed.description = TextFormat::GradientProgressBar.new(
           progress: treasury_item[:count],
-          max: total_needed).render
+          max: total_needed
+        ).render
         embed.thumbnail = { url: item[:icon] }
         embed
       end
 
-      has_enough, not_enough = treasury_item[:needed_by]
+      has_enough, not_enough =
+        treasury_item[:needed_by]
         .partition { |u| u[:count] <= treasury_item[:count] }
-        .collect { |u_list| format_upgrade_list(u_list, upgrades_by_id) }
+        .map { |u_list| format_upgrade_list(u_list, upgrades_by_id) }
 
       has_enough_section = nil
       has_enough_section = "**Enough to Build:**\n#{has_enough}" unless has_enough.empty?
@@ -277,15 +303,19 @@ module GW2
 
     def format_upgrade_list(upgrades, upgrades_by_id)
       upgrades
-        .collect { |u| [u[:count], upgrades_by_id[u[:upgrade_id]][:name]] }
+        .map { |u| [u[:count], upgrades_by_id[u[:upgrade_id]][:name]] }
         .sort
-        .collect { |u| "#{u[1]} :: #{u[0]}" }
+        .map { |u| "#{u[1]} :: #{u[0]}" }
         .join("\n")
     end
 
     attr_reader :guild_name, :item_name, :token
   end
 
+  # An event handler that displays upgrades ready to build for the guild with the given name to the
+  # event's originating channel, using the given GW2 API token.
+  # If Aetherium is the only resource remaining, the upgrade will be displayed with a pending
+  # aetherium count.
   class GuildReadyUpgradesHandler
     def initialize(guild_name, token)
       @guild_name = guild_name
@@ -313,27 +343,30 @@ module GW2
 
       item_ready_upgrade_ids = all_upgrade_ids - incomplete_upgrade_ids
       if item_ready_upgrade_ids.empty?
-        event << "No upgrades ready to build. Get cracking, you slack-tailed maggots! :wrench:"
+        event << 'No upgrades ready to build. Get cracking, you slack-tailed maggots! :wrench:'
         return
       end
 
-      upgrade_ids = item_ready_upgrade_ids.to_a * ","
-      upgrades = GW2::API::GuildUpgradeCatalogMany.request(ids: upgrade_ids)
+      upgrade_ids = item_ready_upgrade_ids.to_a * ','
+      upgrades =
+        GW2::API::GuildUpgradeCatalogMany
+        .request(ids: upgrade_ids)
         .reject do |u|
-          favor_cost = u[:costs].find { |c| c[:name] == "Guild Favor" }
+          favor_cost = u[:costs].find { |c| c[:name] == 'Guild Favor' }
           favor_cost && favor_cost[:count] > guild[:favor]
         end
 
       upgrades.each do |u|
-        aeth_cost = u[:costs].find { |c| c[:name] == "Aetherium" } || {count: 0}
+        aeth_cost = u[:costs].find { |c| c[:name] == 'Aetherium' } || { count: 0 }
         u[:missing_aether] = [0, aeth_cost[:count] - guild[:aetherium]].max
       end
 
-      formatted_upgrades = upgrades
+      formatted_upgrades =
+        upgrades
         .sort_by { |u| u.values_at(:missing_aether, :name) }
-        .collect do |u|
-          aether_info = ""
-          aether_info = " (pending #{u[:missing_aether]} Aetherium)" if u[:missing_aether] > 0
+        .map do |u|
+          aether_info = ''
+          aether_info = " (pending #{u[:missing_aether]} Aetherium)" if u[:missing_aether].positive?
           "#{u[:name]}#{aether_info}"
         end
         .join("\n")
