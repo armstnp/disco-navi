d4317545bdf81f610e6714ff5e1af8cc5a2a202c
Nathan Armstrong <kenogu.labz@gmail.com>
2019-03-22 14:30:20 -0500

    Add Either and Result role object specs

--- spec/disco_navi/recondition_spec.rb
+++ spec/disco_navi/recondition_spec.rb
@@ -78,3 +78,115 @@ describe Recondition::Absent do
     end
   end
 end
+
+describe Recondition::Left do
+  it 'contains a left-slotted value' do
+    Recondition::Left.new 'left'
+  end
+
+  context 'a left value that has been created' do
+    let(:left) { Recondition::Left.new 'left' }
+
+    it 'can be condensed starting with the left case' do
+      condensed_value =
+        left
+        .when_left { |value| value + 'x' }
+        .when_right { |value| value + 'y' }
+
+      expect(condensed_value).to eql 'leftx'
+    end
+
+    it 'can be condensed starting with the right case' do
+      condensed_value =
+        left
+        .when_right { |value| value + 'y' }
+        .when_left { |value| value + 'x' }
+
+      expect(condensed_value).to eql 'leftx'
+    end
+  end
+end
+
+describe Recondition::Right do
+  it 'contains a right-slotted value' do
+    Recondition::Right.new 'right'
+  end
+
+  context 'a right value that has been created' do
+    let(:right) { Recondition::Right.new 'right' }
+
+    it 'can be condensed starting with the left case' do
+      condensed_value =
+        right
+        .when_left { |value| value + 'x' }
+        .when_right { |value| value + 'y' }
+
+      expect(condensed_value).to eql 'righty'
+    end
+
+    it 'can be condensed starting with the right case' do
+      condensed_value =
+        right
+        .when_right { |value| value + 'y' }
+        .when_left { |value| value + 'x' }
+
+      expect(condensed_value).to eql 'righty'
+    end
+  end
+end
+
+describe Recondition::Success do
+  it 'contains a success value' do
+    Recondition::Success.new 'success'
+  end
+
+  context 'a success value that has been created' do
+    let(:success) { Recondition::Success.new 'success' }
+
+    it 'can be condensed starting with the success case' do
+      condensed_value =
+        success
+        .when_success { |value| value + 'x' }
+        .when_failure { |value| value + 'y' }
+
+      expect(condensed_value).to eql 'successx'
+    end
+
+    it 'can be condensed starting with the failure case' do
+      condensed_value =
+        success
+        .when_failure { |value| value + 'y' }
+        .when_success { |value| value + 'x' }
+
+      expect(condensed_value).to eql 'successx'
+    end
+  end
+end
+
+describe Recondition::Failure do
+  it 'contains a failure value' do
+    Recondition::Failure.new 'failure'
+  end
+
+  context 'a failure value that has been created' do
+    let(:failure) { Recondition::Failure.new 'failure' }
+
+    it 'can be condensed starting with the success case' do
+      condensed_value =
+        failure
+        .when_success { |value| value + 'x' }
+        .when_failure { |value| value + 'y' }
+
+      expect(condensed_value).to eql 'failurey'
+    end
+
+    it 'can be condensed starting with the failure case' do
+      condensed_value =
+        failure
+        .when_failure { |value| value + 'y' }
+        .when_success { |value| value + 'x' }
+
+      expect(condensed_value).to eql 'failurey'
+    end
+  end
+end
